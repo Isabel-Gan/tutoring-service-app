e48f9240beb6ec53f2bca21ffe144a19eb5c1b21
U-EMMY\emmal <emmaline.mai@gmail.com>
2020-06-16 16:47:53 -0700

    modified cardviews a bit

---
+++ app/src/main/java/TeachingCardActivity.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tutoring_service_app.R;
+
+public class TeachingCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
